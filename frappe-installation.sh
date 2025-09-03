d86ddf16b6057c84eeabbbc063f31a3d60ee4077
Zaryab03 <qureshizaryab03@gmail.com>
2025-09-03 10:46:46 +0500

    Some fields are deleted.

--- frappe-installation.sh
+++ frappe-installation.sh
@@ -96,80 +96,17 @@ bench new-site --db-name "$DB_NAME" --admin-password "$ADMIN_PASS" "$SITE_NAME" 
 
 cd ../apps/
 bench get-app --branch version-15 https://github.com/sowaan/erpnext.git
-bench get-app --branch main https://github.com/sowaan/leaf_procurement.git 
 cd ../sites/
 bench use erp.abc.com
 bench --site "$SITE_NAME" install-app erpnext
-bench --site "$SITE_NAME" install-app leaf_procurement
 
 # Bench start 
 bench start &
 
 bench --site "$SITE_NAME" migrate --skip-failing
 
-
-
-# scripts and directories setup 
-cd "$DIRECTORY"
-mkdir -p backup/erp.samsons.com logs
-cd "$DIRECTORY"/scripts
-touch backupsite.sh start_bench.sh
-sudo chmod +x backupsite.sh start_bench.sh
-
-
-
-# add backup script data
-sudo tee -a backupsite.sh > /dev/null <<EOL
-#!/bin/bash
-cd /home/frappe/frappe-bench/sites/
-bench --site $SITE_NAME backup --with-files --backup-path "$DIRECTORY"/backup/$SITE_NAME/
-EOL
-
-  
-
-
-# add backup script data
-sudo tee -a start_bench.sh > /dev/null <<EOL
-#!/bin/bash
-sleep 6
-
-# Load NVM manually (critical!)
-export NVM_DIR="/home/frappe/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# Activate Node.js 18
-nvm use 18
-
-echo " "
-echo "ERP STATUS - $(date)"
-echo " "
-
-cd /home/frappe/frappe-bench1
-
-#Checking node version
-node --version
-
-echo " "
-echo "Bench starting..."
-echo "================="
-bench start &
-EOL
-
 timedatectl set-timezone Asia/Karachi
 
-# Nginx installation
-sudo apt-get install -y nginx
-sudo systemctl enable nginx
-sudo systemctl start nginx
-
-# Install and configure Certbot for SSL 
-sudo apt install mkcert -y
-mkcert -install
-
-# Install and configure Avahi for mDNS
-sudo apt install -y avahi-daemon
-sudo systemctl enable avahi-daemon
-sudo systemctl start avahi-daemon
 
 echo " ======================================================================================================="
 echo "Setup completed, please check the scripts in /home/$USER/scripts/ for backup and start bench operations."
